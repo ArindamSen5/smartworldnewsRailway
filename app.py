8aa0117227e152d852176284e4d86fee6a52b8ee
ArindamSen5 <42135437+ArindamSen5@users.noreply.github.com>
2025-11-10 19:27:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import requests
 app = Flask(__name__)
 
 
-NEWSAPI_KEY = os.environ.get('60fd4da6f6804c968a27999e80f50449')
+NEWSAPI_KEY = '60fd4da6f6804c968a27999e80f50449'
 OPENAI_API_KEY = os.environ.get('OPENAI_API_KEY')
 
 def fetch_headlines(query='technology', page_size=10):
